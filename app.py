ed1f609727debe7d9b26a2c40e7618a97fdedff7
Arnold12345 <arnold.schwarzkopf@web.de>
2026-03-31 14:04:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, Response
 import hashlib
 import json
@@ -7,7 +6,7 @@ import os
 app = Flask(__name__)
 
 VERIFICATION_TOKEN = os.getenv("VERIFICATION_TOKEN", "mein_token_1234567890_1234567890")
-ENDPOINT = os.getenv("ENDPOINT", "https://ebay-deletion-endpoint.onrender.com/ebay-notify")
+ENDPOINT = os.getenv("ENDPOINT", "https://ebay-render-endpoint.onrender.com/ebay-notify")
 
 @app.route("/")
 def home():
@@ -19,7 +18,6 @@ def ebay_notify():
         challenge_code = request.args.get("challenge_code", "")
         message = challenge_code + VERIFICATION_TOKEN + ENDPOINT
         challenge_response = hashlib.sha256(message.encode("utf-8")).hexdigest()
-
         body = json.dumps({"challengeResponse": challenge_response}, separators=(",", ":"))
         return Response(body, status=200, mimetype="application/json")
 
